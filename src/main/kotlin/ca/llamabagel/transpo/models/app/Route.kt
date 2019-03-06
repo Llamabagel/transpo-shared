1ec60032a480ee218f075e56af8a5fd804ab4634
derek <derek@llamabagel.ca>
2019-03-06 10:22:27 -0500

    Update Route documentation

--- src/main/kotlin/ca/llamabagel/transpo/models/app/Route.kt
+++ src/main/kotlin/ca/llamabagel/transpo/models/app/Route.kt
@@ -10,13 +10,14 @@ import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
 /**
- * A app route.
+ * Data about a Route derived from GTFS data but simplified for in-client use.
  *
  * @property id The unique id string for this route.
  * @property shortName A short name for this route (typically just the route number)
  * @property longName The long name of the route (usually combined headings?)
- * @property fromHeading The typical heading of the origin point of this route
- * @property toHeading The typical heading of the destination point of this route
+ * @property type The route's type, i.e. rail, bus. 2 indicates rail, 3 indicates bus.
+ * @property serviceLevel The level of service, or how the route is branded (frequent, rapid, local, etc.)
+ * @property color The colour to be used to display this route
  */
 @Entity(tableName = "routes")
 data class Route(
