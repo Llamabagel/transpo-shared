f9b87e23411afd9edce97f945be41c7fa6322882
derek <derek@llamabagel.ca>
2019-07-05 11:26:31 -0400

    Add serializable annotation to AffectedStop

--- shared/src/main/kotlin/ca/llamabagel/transpo/models/updates/AffectedStop.kt
+++ shared/src/main/kotlin/ca/llamabagel/transpo/models/updates/AffectedStop.kt
@@ -4,11 +4,14 @@
 
 package ca.llamabagel.transpo.models.updates
 
+import kotlinx.serialization.Serializable
+
 /**
  * Data class representing a stop that is affected by a service disruption.
  *
  * @property stop The 4-digit code of the stop that is affected
  * @property alternateService The 4-digit code of the stop where alternate service will be provided, if any
  */
+@Serializable
 data class AffectedStop(val stop: String,
                         val alternateService: String? = null)
